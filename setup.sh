a689088612b7928133264b741b042e5e89c1abcc
Alejandro Coy <alejocoy@Alejandros-MacBook-Pro.local>
2020-07-25 20:03:01 -0600

    First app deploy

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"alejocoy17a@gmail.com\"\n\
+echo "[general]
+email = \"alejocoy17a@gmail.com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
+
